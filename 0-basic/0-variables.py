f17ec9e3e6efe1444a8d3ab0446d2b09e9e8c34a
ssilvabl <santiagosblandon@protonmail.com>
2023-03-10 22:21:10 -0500

    initialized repository with variables and git document

---
+++ 0-basic/0-variables.py
@@ -0,0 +1,19 @@
+# VARIABLES 
+
+# Tipos de variables
+
+# int - enteros
+num = 12
+
+# float - flotantes
+num2 = 2.3
+
+# string - cadena de texto
+name = "Santiago"
+
+# bool - booleano
+status = True
+status = False
+
+# Imprimir datos en consola
+print(num1)
